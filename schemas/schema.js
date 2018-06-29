133a512505203bd4f5c78ec731aea47a4ac7f77d
Florian Merle <florian.merle@fiducial.net>
2018-06-29 13:47:13 +0200

    Add basic sqlite models & GraphQL mutations

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,7 +1,9 @@
 const graphql = require('graphql');
+const models = require('../models/index');
 const {
     GraphQLSchema,
     GraphQLObjectType,
+    GraphQLList,
     GraphQLID,
     GraphQLString,
 } = graphql;
@@ -28,9 +30,15 @@ const Query = new GraphQLObjectType({
             type: PlaylistType,
             args: { id: { type: GraphQLID } },
             resolve: (parent, args) => {
-                return { id: 0, name: '80\'s Disco', genre: 'disco' };
+                return models.Playlist.findById(args.id);
             }
-        }
+        },
+        playlists: {
+            type: new GraphQLList(PlaylistType),
+            resolve: (parent, args) => {
+                return models.Playlist.findAll();
+            }
+        },
     }),
 });
 
@@ -38,10 +46,64 @@ const Query = new GraphQLObjectType({
  * TODO:
  * MUTIATION
  */
+const Mutation = new GraphQLObjectType({
+    name: 'Mutations',
+    fields: () => ({
+        addPlaylist: {
+            type: PlaylistType,
+            args: {
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString },
+            },
+            resolve: (parent, args) => {
+                if (!args.name) return;
+
+                return models.Playlist
+                    .build({
+                        name: args.name,
+                        name: args.genre,
+                    })
+                    .save();
+            },
+        },
+        updatePlaylist: {
+            type: PlaylistType,
+            args: {
+                id: { type: GraphQLID },
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString },
+            },
+            resolve: (parent, args) => {
+                if (!args.id) return;
+
+                let data = {};
+                if (args.name) {
+                    data.name = args.name;
+                }
+                if (args.genre) {
+                    data.genre = args.genre;
+                }
+
+                models.Playlist
+                    .update(
+                        data,
+                        {
+                            returning: true,
+                            plain: true,
+                            where: { id: args.id },
+                        }
+                    );
+
+                return models.Playlist.findById(args.id);
+            },
+        },
+    }),
+});
 
  /**
   * EXPORTS
   */
 module.exports = new GraphQLSchema({
-    query: Query
+    query: Query,
+    mutation: Mutation,
 });

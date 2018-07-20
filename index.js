631a3ce01f94a6c830136814633e662fbdfa52f5
florian <florian.david.merle@gmail.com>
2018-07-20 12:37:58 +0200

    Fix cors issues

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 
 require('./models/db');
 const schema = require('./schemas/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', cors(), graphqlHTTP({
     schema,
     graphiql: true
 }));
